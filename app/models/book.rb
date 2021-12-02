4de02b47b39364abe664d5bfa633a8e4c33ad469
Oleksandr Shliakhovyi <aleksandrshliakhovoi@gmail.com>
2021-12-02 02:25:52 +0200

    Add associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
-  validates :author, presence: true, length: { minimum: 3 }
   validates :title, presence: true, length: { minimum: 3 }
+
+  belongs_to :author
 end
